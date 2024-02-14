a128b5ad1ec25ab10692cbbee5fb4fe5b0be996b
xero88 <bernara3@gmail.com>
2024-02-14 07:09:33 +0100

    fix minor bug in country service

--- portail-angular/src/app/services/country.service.ts
+++ portail-angular/src/app/services/country.service.ts
@@ -5,7 +5,7 @@ export class CountryService implements ISelectService {
     async getData(term: string) {
         const result = await fetch(`https://restcountries.com/v3.1/name/${term}`)
         const data = await result.json();
-        const items = data.map((p: any) => ({ title: p.name.common, id: p.name.common }));
+        const items = data?.map((p: any) => ({ title: p.name.common, id: p.name.common }));
 
         return items
     }
